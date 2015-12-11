e346edef9afdb52bef215a8a8371538283aee245
Alessandra Davila <aledavila>
2015-12-11 13:11:48 -0600

    adds double+ digits

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
 $(function() {
-	
+
 	var box = $("input");
-	
+
 	$("#clear").click(function() {
 		box.val(0);
 	});
-	
+
 	$("#zero, #one, #two, #three, #four, #five, #six, #seven, #eight, #nine").click(function(){
 		var n = $(this).val();
+    box.val(box.val() + n);
 	});
-	
-	
+
+
 	$("#plus").click(function(){
-		
+
 	});
 	$("#subtract").click(function(){
-		
+
 	});
 	$("#multiply").click(function(){
-		
+
 	});
 	$("#divide").click(function(){
-		
+
 	});
-	
+
 });
