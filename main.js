9fa0a618a85a3658cbb5f2981a3d32aebbefd108
Brett Wilson <brettwilson7@yahoo.com>
2015-12-11 15:59:08 -0600

    cal_js_final

--- main.js
+++ main.js
@@ -1,28 +1,84 @@
 $(function() {
 
 	var box = $("input");
+	var add, minus, multiply, divide;
+	var num;
 
 	$("#clear").click(function() {
 		box.val('');
 	});
 
 	$("#zero, #one, #two, #three, #four, #five, #six, #seven, #eight, #nine").click(function(){
-		var n = $(this).val();
-    box.val(box.val() + n);
+		box.val(box.val() + $(this).val());
+		
 	});
-
-
 	$("#plus").click(function(){
-
+		add=true;
+		minus=false;
+		multiply=false;
+		divide=false;
+		if(box.val()) {
+			num = parseInt(box.val());
+			box.val('');
+		};
 	});
 	$("#subtract").click(function(){
-
+		add=false;
+		minus=true;
+		multiply=false;
+		divide=false;
+		if(box.val()) {
+			num = parseInt(box.val());
+			box.val('');
+		};
 	});
 	$("#multiply").click(function(){
-
+		add=false;
+		minus=false;
+		multiply=true;
+		divide=false;
+		if(box.val()) {
+			num = parseInt(box.val());
+			box.val('');
+		};
 	});
 	$("#divide").click(function(){
-
+		add=false;
+		minus=false;
+		multiply=false;
+		divide=true;
+		if(box.val()) {
+			num = parseInt(box.val());
+			box.val('');
+		};
 	});
-
+	$("#equals").click(function(){
+		var num2 = parseInt(box.val());
+		if(num && add) {
+			box.val(num + num2);
+		} else if (num && minus) {
+			box.val(num - num2);
+		} else if (num && multiply) {
+			box.val(num * num2);
+		} else if (num && divide) {
+			box.val(num / num2);
+		} else {
+			//$(body).insertBefore("img", );
+		}
+	});
 });
+
+//numbers gets clicked and box.val needs to get stored
+//operator gets clicked and that gets stored
+//numbers get clicked again and box.val must get stored
+//user clicks equals, 1st, 2nd set of numbers get operated on by stored operator
+
+//box.val().substring(box.val().length) === this.val() ? box.val() + return false;
+
+
+
+
+
+
+
+
