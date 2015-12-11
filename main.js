ccd8ae171eb4b4d95817fdc5b06931992511aee5
Brett Wilson <brettwilson7@yahoo.com>
2015-12-11 10:52:43 -0600

    js main number variables

---
+++ main.js
@@ -0,0 +1,30 @@
+$(function() {
+	var btn_one = $("#one").click(function(){
+		
+	});
+	var btn_two = $("#two").click(function(){
+		
+	});
+	var btn_three = $("#three").click(function(){
+		
+	});
+	var btn_four = $("#four").click(function(){
+		
+	});
+	var btn_five = $("#five").click(function(){
+		
+	});
+	var btn_six = $("#six").click(function(){
+		
+	});
+	var btn_seven = $("#seven").click(function(){
+		
+	});
+	var btn_eight = $("#eight").click(function(){
+		
+	});
+	var btn_nine = $("#nine").click(function(){
+		
+	});
+	
+});
